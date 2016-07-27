87eec8202c375440bea4725a9c24c5048357fc1b
rakesh <rakeshkamboj1826@gmail.com>
2016-07-27 14:48:12 +0530

    Region drop down changes
    Change-Id: I9400dae1a9dcde09368c4733ad71bf526dd94585
    Signed-off-by: rakesh <rakeshkamboj1826@gmail.com>

--- aws-demo-api/src/main/webapp/WEB-INF/view/resources/js/cmsDashBoard.js
+++ aws-demo-api/src/main/webapp/WEB-INF/view/resources/js/cmsDashBoard.js
@@ -3,75 +3,80 @@
 		  
 		  cmsModule.controller('CMSController', function($scope) {
 	
-		  $scope.teams = {
-                    'CMS': {
-                        'GoPro': ['Dev', 'QA', 'PRD'],
-                        'WWE': ['Dev', 'QA', 'PRD'],  
-                    },
-                    'CMSBUS': {
-                      'ARINA': ['Dev', 'QA', 'PRD'],
-                      'HBO': ['Dev', 'QA', 'PRD']
-                    },
-                    'CMSUI': {
-                        'Times': ['Dev', 'QA', 'PRD'],
-                        'TvToday': ['Dev', 'QA', 'PRD']
-                    }
+		  $scope.regions = {
+                    'Asia Pacific':{
+							'CMS': {
+								'GoPro': ['Dev', 'QA', 'PRD'],
+								'WWE': ['Dev', 'QA', 'PRD'],  
+								},
+								'CMSBUS': {
+								  'ARINA': ['Dev', 'QA', 'PRD'],
+								  'HBO': ['Dev', 'QA', 'PRD']
+								},
+								'CMSUI': {
+									'Times': ['Dev', 'QA', 'PRD'],
+									'TvToday': ['Dev', 'QA', 'PRD']
+								}
+						},
+					'EU':{
+						'CMS': {
+								'GoPro': ['Dev', 'QA', 'PRD'],
+								'WWE': ['Dev', 'QA', 'PRD'],  
+								}
+						}
                 };
 			 
 			 $scope.columnDefs = [
 					{ name:'instanceId', displayName:'Instance ID', width:100 },
 					{ name:'privateIP',displayName:'Private IP', width:100, pinnedLeft:true },
 					{ name:'state', width:150 },
-					{ name:'tags[0].Team', displayName:'Team', width:150 },
-					{ name:'tags[1].Partner', displayName:'partner', width:150 },
-					{ name:'tags[2].env', displayName:'Environment', width:150 },
+					{ name:'tags[0].region', displayName:'Region', width:150 },
+					{ name:'tags[1].Team', displayName:'Team', width:150 },
+					{ name:'tags[2].Partner', displayName:'partner', width:150 },
+					{ name:'tags[3].env', displayName:'Environment', width:150 },
 				];	
 			
 				 
-			$scope.teamFilter = function() {
-				alert( document.getElementById("team").value);
-				
+			$scope.regionFilter = function() {
+				//alert( document.getElementById("region").value);				
 				$scope.gridOptions = [ {
 					"instanceId": "123",
 					"privateIP": "wwe",
 					"state":"Active",
-					"tags":[{"Team":"cms"},{"Partner":"HBO"},{"env":"dev"}]
+					"tags":[{"region":"Asia Pacific"},{"Team":"cms"},{"Partner":"HBO"},{"env":"dev"}]
 				},
 				{
 					"instanceId": "345",
 					"privateIP": "hbohome",
 					"state": "Active",
-					"tags":[{"Team":"cms"},{"Partner":"GoPro"},{"env":"dev"}]
+					"tags":[{"region":"Asia Pacific"},{"Team":"cms"},{"Partner":"GoPro"},{"env":"dev"}]
 				},
 				{
 					"instanceId": "789",
 					"privateIP": "godaday",
 					"state":"Active",
-					"tags":[{"Team":"cms"},{"Partner":"Arena"},{"env":"dev"}]
+					"tags":[{"region":"Asia Pacific"},{"Team":"cms"},{"Partner":"Arena"},{"env":"dev"}]
 				},
 				{
 					"instanceId": "345",
 					"privateIP": "hbohom11e",
 					"state":"Active",
-					"tags":[{"Team":"cms"},{"Partner":"wwe"},{"env":"dev"}]
+					"tags":[{"region":"EU"},{"Team":"cms"},{"Partner":"wwe"},{"env":"dev"}]
 				}
 				];	
 				
 			};
-			
+			$scope.teamFilter = function() {
+				//alert( document.getElementById("region").value);
+				
+			};
 			$scope.partnerFilter = function() {
-				alert( document.getElementById("partner").value);
+				//alert( document.getElementById("partner").value);
 				
 			};
 			$scope.envFilter = function() {
-				alert( document.getElementById("env").value);
+				//alert( document.getElementById("env").value);
 				
 			};
-			
-			
-			
-			
-			
-		
-			});
+});
 		
