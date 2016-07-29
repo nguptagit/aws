bdb561992d9b2d9ee61f65708bddbc8c293a8a20
ashish.pandey <ashish.pandey@Ashish-Pandey.in.corp.tavant.com>
2016-07-29 09:55:24 +0530

    remove hard code data

--- aws-demo-api/src/main/java/com/aws/demo/services/impl/DropDownServiceImpl.java
+++ aws-demo-api/src/main/java/com/aws/demo/services/impl/DropDownServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 import com.aws.demo.constant.AwsConstant;
+import com.aws.demo.constant.CharConstant;
 import com.aws.demo.services.DropDownService;
 
 @Configuration
@@ -26,6 +27,10 @@ public class DropDownServiceImpl implements DropDownService{
 	@Resource(name="awsProp")
 	private Properties awsRegionProperty;
 
+	private static final String DROP_DOWN_DATA = "dropDowndata";
+	private static final String AWS_REGION = "awsRegion";
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONObject getDashBoardDropDownData() {
 		JSONObject jsonObject= new JSONObject();
@@ -33,34 +38,28 @@ public class DropDownServiceImpl implements DropDownService{
 		String team = env.getProperty(AwsConstant.TEAM);
 		Map<String, Map<String, String[]>> dropdownMap = new HashMap<String,Map<String, String[]>>();
 		if (team != null ) {
-			String teams [] = team.split(",");
+			String teams [] = team.split(CharConstant.COMMA);
 			for (String teamName : teams) {
 				String partner = env.getProperty(teamName);
 				Map<String, String[]> partnerMap = new HashMap<String, String[]>();
-				String partners[] = partner.split(",");
+				String partners[] = partner.split(CharConstant.COMMA);
 				for (String partnerName : partners ){
-					String[] envrionment = env.getProperty(partnerName).split(",");
+					String[] envrionment = env.getProperty(partnerName).split(CharConstant.COMMA);
 					partnerMap.put(partnerName, envrionment);
 				}
 				dropdownMap.put(teamName, partnerMap);
 			}
 		}
-		jsonObject.put("dropDowndata", dropdownMap);
+		jsonObject.put(DROP_DOWN_DATA, dropdownMap);
 		
 		Map<String,String> awsKeysSet=new HashMap<String,String>();
 		if(awsRegionProperty!=null){
 		Set<Object> awsKeys=awsRegionProperty.keySet();
-		
 		for (Object key : awsKeys) {
 			awsKeysSet.put((String)key, awsRegionProperty.getProperty((String)key));
-		}
-			
-		}
-		
-		jsonObject.put("awsRegion", awsKeysSet);
-		
-		
-		
+		 }
+		}	
+		jsonObject.put(AWS_REGION, awsKeysSet);	
 		return jsonObject;
 	}
 
